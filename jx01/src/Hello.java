4d75f3a47e8706aa81f57024285dd0ed2e9fc3f2
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-02 15:31:02 +0900

    :sparkles: Hello.javaを作成した

---
+++ jx01/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        String name = "Hiroya Watanabe";
+        String studentNum = "18122508";
+        int grade = 2;
+        System.out.println("Name: " + name);
+        System.out.println("Student number: " + studentNum);
+        System.out.println("Grade: "  + grade);
+    }
+}
