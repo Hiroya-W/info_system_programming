a8388e7bb39811eaf42dc662881b570d6e942578
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-09 10:03:23 +0900

    :sparkles: Dogクラスを追加

---
+++ jx02/src/Dog.java
@@ -0,0 +1,22 @@
+public class Dog {
+    // インスタンス変数
+    private String name;
+    private String voice;
+
+    // メソッド
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+}
