63724ab3b85167b2fb098fc2e8553663db714177
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-09 13:10:34 +0900

    :sparkles: Personクラスを追加

---
+++ jx02/src/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+    private String lastName;
+    private String firstName;
+    private String address;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lname) {
+        this.lastName =  lname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fname) {
+        this.firstName = fname;
+    }
+
+    public String getName(){
+        return getFirstName() + " " + getLastName();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String add) {
+        this.address =  add;
+    }
+
+    public void print(){
+        System.out.println("Name: " + getName());
+        System.out.println("Address: " + getAddress());
+    }
+}
