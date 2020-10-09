d350387eda271e2b9118de872ee60cdb9dfe23d7
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-09 13:48:39 +0900

    :sparkles: Factorialクラスを追加

---
+++ jx02/src/Factorial.java
@@ -0,0 +1,20 @@
+public class Factorial {
+    private int num;
+    private int fact;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int n) {
+        this.num = n;
+    }
+
+    public int getFact() {
+        return fact;
+    }
+
+    public void setFact(int n) {
+        this.fact = n;
+    }
+}
