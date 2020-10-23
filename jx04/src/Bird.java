458ce3d1ce3713d88db91ff7098be96d9b3e0d94
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-10-23 13:49:02 +0900

    :sparkles: mainメソッドを用意した

--- jx04/src/Bird.java
+++ jx04/src/Bird.java
@@ -17,4 +17,17 @@ public class Bird {
     public static int genNum() {
         return numBird;
     }
+
+    public static void main(String[] args) {
+        Bird instBird = new Bird();
+        Chicken instChicken1 = new Chicken();
+        Chicken instChicken2 = new Chicken();
+        Penguin instPenguin1 = new Penguin();
+        Penguin instPenguin2 = new Penguin();
+        Penguin instPenguin3 = new Penguin();
+
+        System.out.println("Bird: " + Bird.genNum());
+        System.out.println("Chicken: " + Chicken.getNum());
+        System.out.println("Penguin: " + Penguin.getNum());
+    }
 }
